f669d5161642def91c6a9c8b27978d1ae514c20c
VanderStack <zach@vanderstack.com>
2019-03-18 17:17:42 -0700

    Added EF Core LocalDb Database VanderStackHealthCatalyst
    Configured initial migration to automatically scaffold Db on new machine.

---
+++ HealthCatalystPeopleSearch/Migrations/20190315175013_InitialCreate.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace VanderStack.HealthCatalystPeopleSearch.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PersonSet",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Address = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    PhotoUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonSet", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InterestModel",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    PersonId = table.Column<Guid>(nullable: false),
+                    PersonModelId = table.Column<Guid>(nullable: true),
+                    Summary = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InterestModel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InterestModel_PersonSet_PersonModelId",
+                        column: x => x.PersonModelId,
+                        principalTable: "PersonSet",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InterestModel_PersonModelId",
+                table: "InterestModel",
+                column: "PersonModelId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InterestModel");
+
+            migrationBuilder.DropTable(
+                name: "PersonSet");
+        }
+    }
+}
---
+++ HealthCatalystPeopleSearch/PersonFeature/PersonDatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VanderStack.HealthCatalystPeopleSearch.PersonFeature
+{
+    public class PersonDatabaseContext
+		: DbContext
+	{
+		public PersonDatabaseContext(DbContextOptions<PersonDatabaseContext> options)
+		: base(options)
+		{ }
+
+		public DbSet<PersonModel> PersonSet { get; set; }
+	}
+}
